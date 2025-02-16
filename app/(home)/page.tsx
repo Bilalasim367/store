a3d761e4e089691926b6c858936cd0749365ad26
Bilalasim367 <malikhussian224266@gmail.com>
2025-02-16 11:10:47 +0500

    10

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -9,6 +9,7 @@ import { toSlug } from '@/lib/utils'
 import data from '@/lib/data'
 import ProductSlider from '@/components/shared/product/product-slider'
 import { Card, CardContent } from '@/components/ui/card'
+import BrowsingHistoryList from '@/components/shared/browsing-history-list'
 
 export default async function Page() {
   const todaysDeals = await getProductsByTag({ tag: 'todays-deal' })
@@ -87,6 +88,9 @@ export default async function Page() {
           </CardContent>
         </Card>
       </div>
+      <div className='p-4 bg-background'>
+  <BrowsingHistoryList />
+</div>
     </>
   )
 }
